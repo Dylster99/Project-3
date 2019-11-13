e371c8198a0afd24cfb8b63902bb58354300cfbd
Dylan Ferragame <50226788+Dylster99@users.noreply.github.com>
2019-11-12 23:32:13 -0700

    changed mlab stuff

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
 }
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds041248.mlab.com:41248/heroku_q3w93hkn");
 
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
