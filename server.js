392e7aa947d8b4928ef25d90321b53e9d1133c36
Dylan Ferragame <50226788+Dylster99@users.noreply.github.com>
2019-11-13 02:18:45 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 
+const session = require("express-session");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 var app = express();
@@ -13,6 +14,16 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.use(require('serve-static')(__dirname + '/../../public'));
+
+app.use(
+    require('express-session')({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: false
+    }));
+  
 app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://user:password1@ds041248.mlab.com:41248/heroku_q3w93hkn");
